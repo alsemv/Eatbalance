05266db2ec7e46b9194247c33cc072547d2f9975
Alexander Semenev <dfmdeveloper2@lyb.it>
2018-05-07 19:03:20 +0300

    add console command for change users role

---
+++ app/Console/Commands/User/RoleCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands\User;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class RoleCommand extends Command
+{
+    protected $signature = 'user:role {email} {role}';
+    protected $description = 'Set role for user by email';
+
+    public function handle(): bool
+    {
+        $email = $this->argument('email');
+        $role = $this->argument('role');
+
+        if (!$user = User::where('email', $email)->first()) {
+            $this->error('Нет пользователя с таким мейлом ' . $email);
+            return false;
+        }
+
+        try {
+            $user->changeRole($role);
+        } catch (\DomainException $e) {
+            $this->error($e->getMessage());
+            return false;
+        }
+
+        $this->info('Роль успешно изменена');
+        return true;
+    }
+}
